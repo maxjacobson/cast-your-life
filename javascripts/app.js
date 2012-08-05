ec30924733860039a8afd7e13750542b3a39da3c
Platon Dickey <pdickey@ltutech.com>
2012-08-04 22:17:16 -0400

    first commit

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,100 @@
 var App = Em.Application.create({
-  title: '',
   ready: function() {
-    document.title = this.get('title');
-    $('.page-header .title').html(this.get('title'));
   }
 });
+
+/********
+ * Data *
+ ********/
+
+App.Movie = DS.Model.extend({
+  title: DS.attr('string'),
+  people: DS.hasMany('App.Person')
+});
+
+App.Person = DS.Model.extend({
+  name: DS.attr('string'),
+  actor_name: DS.attr('string'),
+  actor_image: DS.attr('string')
+});
+
+App.Movie.FIXTURES = [
+  {
+    id: 1,
+    title: 'Soul Train'
+  },
+  {
+    id: 2,
+    title: 'Max Jacobson and the Horrible Lights'
+  }
+];
+
+// App.Adapter = DS.RESTAdapter.extend({
+
+// });
+
+App.store = DS.Store.create({
+  revision: 4,
+  adapter: DS.FixtureAdapter.create()
+});
+
+
+App.tmdb = DS.
+
+/***********************
+ * Controllers & Views *
+ ***********************/
+
+App.ApplicationController = Em.Controller.extend({});
+App.ApplicationView = Em.View.extend({
+  templateName: 'application'
+});
+
+App.MoviesController = Em.ArrayController.extend({});
+App.MoviesView = Em.View.extend({
+  templateName: 'movies'
+});
+
+App.MovieController = Em.ObjectController.extend({});
+App.MovieView = Em.View.extend({
+  templateName: 'movie'
+});
+
+App.PeopleController = Em.ArrayController.extend({});
+
+
+/**********
+ * Router *
+ **********/
+
+App.Router = Em.Router.extend({
+  enableLogging: true,
+  location: 'hash',
+
+  root: Em.Route.extend({
+
+    index: Em.Route.extend({
+      route: '/',
+      redirectsTo: 'movies.list'
+    }),
+
+    movies: Em.Route.extend({
+      route: '/movies',
+      list: Em.Route.extend({
+        route: '/',
+        connectOutlets: function(router) {
+          router.get('applicationController').connectOutlet('movies', App.Movie.find());
+        }
+      }),
+      showMovie: Em.Route.transitionTo('movie'),
+      movie: Em.Route.extend({
+        route: '/:movie_id',
+        connectOutlets: function(router, movie) {
+          router.get('applicationController').connectOutlet('movie', movie);
+        }
+      })
+    })
+  })
+})
+
+App.initialize();
