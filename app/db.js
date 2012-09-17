b5470ba9d65f60e58647a29c01b99e37d1ac74f3
Platon <platon.dickey@gmail.com>
2012-09-17 01:07:18 -0400

    enabled generated IDs

--- app/db.js
+++ app/db.js
@@ -4,29 +4,49 @@ var async = require('async'),
 var pg_client = new pg.Client('postgres://cast:eleluku@pauldechov.com:5432/cast');
 pg_client.connect();
 
+
+// TODO generate random base62 number (5 digit?) represented with [0-9A-Za-z]
+var id_index = 1;
 var generateID = function() {
-  // generate 5 digit base62 string and convert to [0-9A-Za-z]
-  // use as movie and member ids instead of serial columns
+  return 'gen' + (id_index++);
+};
+var generateUniqueID = function(table_name, callback) {
+  var id = null,
+      is_unique = false;
+  async.until(
+    function() { return is_unique; },
+    function(callback) {
+      id = generateID();
+      pg_client.query('SELECT COUNT(*) FROM '+table_name+' WHERE id = $1', [id], function(err, result) {
+        if (err) { return callback(err); }
+        is_unique = result.rows[0].count === 0;
+        return callback();
+      });
+    },
+    function(err) {
+      if (err) { return callback(err); }
+      return callback(null, id);
+    }
+  );
 };
 
-var resetTables = function(callback) {
+var reset = function(callback) {
   async.forEachSeries([
     'DROP TABLE IF EXISTS movies, members, movies_to_members',
-    'CREATE TABLE movies (id serial PRIMARY KEY, title varchar(255))',
-    'CREATE TABLE members (id serial PRIMARY KEY, movie_id integer REFERENCES movies(id), friend_name varchar(255), friend_fb_id integer, actor_name varchar(255), actor_tmdb_id integer, actor_image_url varchar(255))'
+    'CREATE TABLE movies (id varchar(8) PRIMARY KEY, title varchar(255))',
+    'CREATE TABLE members (id varchar(8) PRIMARY KEY, movie_id varchar(8) REFERENCES movies(id), friend_name varchar(255), friend_fb_id integer, actor_name varchar(255), actor_tmdb_id integer, actor_image_url varchar(255))'
   ], function(query_string, callback) {
     pg_client.query(query_string, callback);
   }, callback);
 };
 
 var createMovie = function(callback) {
-  pg_client.query('INSERT INTO movies DEFAULT VALUES RETURNING id', function(err, result) {
-    callback(err, result.rows[0]);
-  });
-};
-var createMovieFromTitle = function(title, callback) {
-  pg_client.query('INSERT INTO movies (title) VALUES ($1) RETURNING id, title', [title], function(err, result) {
-    callback(err, result.rows[0]);
+  generateUniqueID('movies', function(err, id) {
+    if (err) { return callback(err); }
+    console.log('inserting movie:', id);
+    pg_client.query('INSERT INTO movies (id) VALUES ($1) RETURNING id', [id], function(err, result) {
+      callback(err, result.rows[0]);
+    });
   });
 };
 
@@ -48,8 +68,12 @@ var getMovie = function(id, callback) {
 };
 
 var createMember = function(member, callback) {
-  pg_client.query('INSERT INTO members (movie_id, friend_name, actor_name, actor_image_url) VALUES ($1, $2, $3, $4) RETURNING id, movie_id, friend_name, actor_name, actor_image_url', [member.movie_id, member.friend_name, member.actor_name, member.actor_image_url], function(err, result) {
-    callback(err, result.rows[0]);
+  generateUniqueID('members', function(err, id) {
+    if (err) { return callback(err); }
+    console.log('inserting member:', id);
+    pg_client.query('INSERT INTO members (id, movie_id, friend_name, actor_name, actor_image_url) VALUES ($1, $2, $3, $4, $5) RETURNING id, movie_id, friend_name, actor_name, actor_image_url', [id, member.movie_id, member.friend_name, member.actor_name, member.actor_image_url], function(err, result) {
+      callback(err, result.rows[0]);
+    });
   });
 };
 var deleteMember = function(id, callback) {
@@ -57,24 +81,8 @@ var deleteMember = function(id, callback) {
 };
 
 
-var initialize = function() {
-  resetTables(function(err) {
-    if (err) { return console.error(err); }
-    console.log('tables reset.');
-    async.forEachSeries([
-      'Soul Train',
-      'Max Jacobson and the Horrible Lights',
-      'The Jacobson Family'
-    ], createMovieFromTitle, function(err) {
-      if (err) { return console.error(err); }
-      console.log('fixtures loaded.');
-    });
-  });
-};
-
-
 module.exports = {
-  initialize: initialize,
+  reset: reset,
   getMovie: getMovie,
   createMovie: createMovie,
   updateMovie: updateMovie,
